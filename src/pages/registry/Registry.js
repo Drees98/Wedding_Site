9fdee5ca9e97e83740498aa7adc178ae866a4316
Drees98 <davidrees191@gmail.com>
2025-09-07 22:22:32 -0700

    Small updates and spelling corrections

--- src/pages/registry/Registry.js
+++ src/pages/registry/Registry.js
@@ -2,7 +2,7 @@ import "./Registry.css"
 function Registry() {
     return(
         <div>
-            <h1>Please know that your pressence at our wedding is present enough! However, if you need some gift idease plese check out our registry below.</h1>
+            <h1>Please know that your presence at our wedding is present enough! However, if you need some gift ideas please check out our registry below.</h1>
             <link to="https://amazon.ca/wedding/registry/ZWJ2UA6DQD4Z" target="_blank" rel="noopener noreferrer" />
             <a className="button-link" href="https://amazon.ca/wedding/registry/ZWJ2UA6DQD4Z" target="_blank" rel="noreferrer">Registry</a>
         </div>
--- src/pages/rsvp/RSVP.js
+++ src/pages/rsvp/RSVP.js
@@ -6,29 +6,48 @@ function RSVP() {
         setAdditionalGuests(val > 1)
     }
 
+    const [canCome, setCanCome] = useState(true)
+    const changeCanCome = () => {
+        setCanCome(!canCome)
+    }
+
     function handleFormSubmit(event) {
         event.preventDefault()
         const data = event.target
-        var subject = encodeURIComponent("We will be there")
-        var guests = encodeURIComponent("List of guests: " + data.name.value + " " + (additionalGuests ? data.additionalNames.value : "") + "\n\n")
+        var subject = ""
+        var body = ""
+        var guests = ""
         var numberOfAdults = encodeURIComponent("Guests of legal drinking age: " + data.adultCount.value + "\n\n")
-        var accomodations = encodeURIComponent(!data.accomodations.value ? "": "Accomodations we will need include: " + data.accomodations.value + "\n\n")
+        var accomodations = encodeURIComponent(!data.accomodations.value ? "": "Accommodations we will need include: " + data.accomodations.value + "\n\n")
         var roomNeeded = encodeURIComponent(data.needRoom.checked ? "We will need to book a room\n\n" : "")
         var comment = encodeURIComponent(data.additionalComments.value)
-        var body = guests + numberOfAdults + accomodations + roomNeeded + comment
+        if(canCome)
+        {
+            subject = encodeURIComponent("We will be there")
+            guests = encodeURIComponent("List of guests: " + data.name.value + " " + (additionalGuests ? data.additionalNames.value : "") + "\n\n")
+            body = guests + numberOfAdults + accomodations + roomNeeded + comment
+        }
+        else{
+            subject = encodeURIComponent("We will not be there")
+            guests = encodeURIComponent("Name of party: "  + data.name.value + "\n\n")
+            body = guests + encodeURIComponent("Sorry, but we will not be able to make it\n\n")  + comment
+        }
         window.location.href = "mailto:david.jasmine.rees@gmail.com?subject=" + subject + "&body=" + body
     }
 
     return(
         <div id="RSVP" className="tabContent RSVP hidden-tab">
             <form onSubmit={(event) => handleFormSubmit(event)}>
-                <label>Name: <input type="text" name="name" required/></label>
-                <label>Total number guests: <input type="number" name="plus1count" min="1" defaultValue={1} onChange={(e) => changeCount(Number(e.target.value))}/></label>
-                <label className={additionalGuests ? 'show-content': 'hide-content'}>Names of additional guests (first and last for seating): <textarea name="additionalNames"/></label>
-                <label>Number of adults in the party (19+): <input type="number" name="adultCount" min="0" defaultValue={0}/></label>
-                <label>Dietary Restrictions/Physical Accomodations: <textarea name="accomodations"/></label>
-                <label>Do you need a hotel room? <input className='checkbox-style' type="checkbox" name="needRoom"/></label>
-                <label>Additional comments: <textarea name="additionalComments"/></label>
+                <label><span className='RSVP-label-text'>Will you be joining us? </span><input className='checkbox-style' type="checkbox" name="canComeRoom" defaultChecked={true} onClick={changeCanCome}/></label>
+                <label><span className='RSVP-label-text'>Name: </span><input type="text" name="name" required/></label>
+                <div className={canCome ? 'show-content': 'hide-content'}>
+                    <label><span className='RSVP-label-text'>Total number guests: </span><input className='RSVP-numfield' type="number" name="plus1count" min="1" defaultValue={1} onChange={(e) => changeCount(Number(e.target.value))}/></label>
+                    <label className={additionalGuests ? 'show-content': 'hide-content'}><span className='RSVP-label-text'>Names of additional guests<br className='RSVP-mobile-break'/> (first and last for seating): </span><textarea name="additionalNames"/></label>
+                    <label><span className='RSVP-label-text'>Number of adults in the party (19+): </span><input className='RSVP-numfield' type="number" name="adultCount" min="0" defaultValue={0}/></label>
+                    <label><span className='RSVP-label-text'>Dietary Restrictions/Physical Accommodations: </span><textarea name="accomodations"/></label>
+                    <label><span className='RSVP-label-text'>Do you need a hotel room? </span><input className='checkbox-style' type="checkbox" name="needRoom"/></label>
+                </div>
+                <label><span className='RSVP-label-text'>Additional comments: </span><textarea name="additionalComments"/></label>
                 <input className='RSVP-button' type="submit" value="submit"/>
             </form>
             <p className='RSVP-info'>*Please hit send on the auto generated email for us to receive your RSVP</p>
